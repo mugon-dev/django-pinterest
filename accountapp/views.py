9cd5bd416dabb54b6445582a3970173d513e3737
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-05-20 16:47:45 +0900

    course 20 create user, login, logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
-
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
 
@@ -24,3 +26,13 @@ def hello_world(request):
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
 
+
+class AccountCreateView(CreateView):
+    # 장고에서 제공하는 기본 user 모델
+    model = User
+    # 장고에서 제공하는 템플릿
+    form_class = UserCreationForm
+    # 계정 생성 성공시 이동하는 url
+    success_url = reverse_lazy('accountapp:hello_world')
+    # 회원가입을 할때 어떤 html 파일을 볼지
+    template_name = 'accountapp/create.html'
