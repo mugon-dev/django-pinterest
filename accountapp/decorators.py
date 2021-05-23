745bee03ef6350c9f1284d0e9e95364ce38d6a52
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-05-23 20:08:46 +0900

    course 33 add article app, magicgrid

--- accountapp/decorators.py
+++ accountapp/decorators.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseForbidden
 
 def account_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        user = User.object.get(pk=kwargs["pk"])
+        user = User.objects.get(pk=kwargs["pk"])
         if not user == request.user:
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
