a45bf31964164b4a0455c3aabb12bcf689c11fe9
MG_dev <68521263+mugon-dev@users.noreply.github.com>
2021-05-24 06:51:06 +0900

    course 46 fix bug, style

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from accountapp.views import (
-    hello_world,
     AccountCreateView,
     AccountDetailView,
     AccountUpdateView,
@@ -11,7 +10,6 @@ from accountapp.views import (
 app_name = "accountapp"
 
 urlpatterns = [
-    path("hello_world/", hello_world, name="hello_world"),
     path(
         "login/", LoginView.as_view(template_name="accountapp/login.html"), name="login"
     ),
